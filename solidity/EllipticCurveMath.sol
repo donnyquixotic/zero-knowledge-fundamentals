f92a211aabd0e1f0853f6ee2c26c6a2738e429ca
Don <don@caleos.io>
2023-11-26 13:59:14 -0600

    add bilinear pairing precompile methods

--- solidity/EllipticCurveMath.sol
+++ solidity/EllipticCurveMath.sol
@@ -57,6 +57,24 @@ library math {
             result := mload(value)
         }
     }
+
+    function run(uint256[12] memory input) public view returns (bool) {
+        assembly {
+            let success := staticcall(gas(), 0x08, input, 0x0180, input, 0x20)
+            if success {
+                return(input, 0x20)
+            }
+        }
+        revert("Wrong pairing");
+    }
+
+    //function run(bytes calldata input) public view returns (bool) {
+    //    // optional, the precompile checks this too and reverts (with no error) if false, this helps narrow down possible errors
+    //    if (input.length % 192 != 0) revert("Points must be a multiple of 6");
+    //    (bool success, bytes memory data) = address(0x08).staticcall(input);
+    //    if (success) return abi.decode(data, (bool));
+    //    revert("Wrong pairing");
+    //}
 }
 
 contract EllipticCurveMath {
